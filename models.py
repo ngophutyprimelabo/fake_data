e5917057872084b38628c74389a798158dc62ca2
tynp <ty@example.com>
2025-03-31 16:01:27 +0700

    fix model

--- models.py
+++ models.py
@@ -17,12 +17,12 @@ class Message(Base):
     conversation_id: Mapped[int] = mapped_column(
         Integer, ForeignKey("conversations.external_id"), nullable=False, index=True
     )
-    message: Mapped[str] = mapped_column(Text, nullable=False)
+    message: Mapped[str] = mapped_column(LONGTEXT, nullable=False)
     is_bot: Mapped[bool] = mapped_column(Boolean, nullable=False, index=True)
     main_category: Mapped[str] = mapped_column(String(255), nullable=True)
     category_group: Mapped[str] = mapped_column(String(255), nullable=True)
     chat_parameter_category: Mapped[str] = mapped_column(String(255), nullable=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False ,index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, index=True)
 
 
 class Conversation(Base):
@@ -67,7 +67,7 @@ class CategoryMapping(Base):
     __tablename__ = "category_mappings"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    category_group: Mapped[int] = mapped_column(Integer, nullable=False)
+    category_group: Mapped[str] = mapped_column(String(5), nullable=False)
     category_group_label: Mapped[str] = mapped_column(String(255), nullable=False)
     main_category: Mapped[str] = mapped_column(String(5), nullable=False)
     main_category_label: Mapped[str] = mapped_column(String(255), nullable=False)
@@ -76,7 +76,9 @@ class CategoryMapping(Base):
         String(255), nullable=False
     )
     created_at: Mapped[datetime] = mapped_column(
-        DateTime, server_default=func.now(), nullable=False,
+        DateTime,
+        server_default=func.now(),
+        nullable=False,
     )
 
 
@@ -116,11 +118,14 @@ class Personnel(Base):
     )
     entry_year: Mapped[Optional[int]] = mapped_column(Integer)
     department_code: Mapped[str] = mapped_column(
-        String(5), ForeignKey("organizations.external_department_code"), nullable=False, index=True
+        String(5),
+        ForeignKey("organizations.external_department_code"),
+        nullable=False,
+        index=True,
     )
     branch_code: Mapped[Optional[str]] = mapped_column(String(3))
     head_office_name: Mapped[Optional[str]] = mapped_column(String(255))
-    branch_name: Mapped[Optional[str]] = mapped_column(String(255), nullable=False)
+    branch_name: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
     section_name: Mapped[Optional[str]] = mapped_column(String(255))
     sales_office_name: Mapped[Optional[str]] = mapped_column(String(255))
     organization_type: Mapped[Optional[str]] = mapped_column(String(255))
@@ -147,7 +152,7 @@ class Organization(Base):
     )
     external_division_code: Mapped[str] = mapped_column(String(3), nullable=False)
     external_section_code: Mapped[str] = mapped_column(String(2), nullable=False)
-    field: Mapped[Optional[str]] = mapped_column(String(255) , index=True)  
+    field: Mapped[Optional[str]] = mapped_column(String(255), index=True)
     field_detail: Mapped[Optional[str]] = mapped_column(String(255), index=True)
     region: Mapped[Optional[str]] = mapped_column(String(255))
     branch: Mapped[Optional[str]] = mapped_column(String(255))
@@ -166,7 +171,9 @@ class FieldMapping(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     field: Mapped[str] = mapped_column(String(255), nullable=False, index=True)
-    field_detail: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
+    field_detail: Mapped[str] = mapped_column(
+        String(255), unique=True, nullable=False, index=True
+    )
 
     @property
     def field_mapping(self):
@@ -198,3 +205,13 @@ class Abbreviation(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     abbreviation: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+
+class Deparment(Base):
+    _tablename__ = "departments"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    external_department_code: Mapped[str] = mapped_column(
+        String(5), nullable=False
+    )
+    branch: Mapped[str] = mapped_column(String(255), nullable=False)
+    abbreviation: Mapped[str] = mapped_column(String(255), nullable=False)
